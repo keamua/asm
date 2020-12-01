8886ee4e3afd8287fd97a5c21c70d383e2899426
keamua <67619183+keamua@users.noreply.github.com>
2020-12-01 21:36:16 +0800

    Update tree.h
    改了遍历函数

--- tree.h
+++ tree.h
@@ -2,6 +2,7 @@
 #define TREE_H
 #include<iostream>
 #include<string>
+#include<queue>
 
 using std::cerr;
 using std::cout;
@@ -32,6 +33,9 @@ enum OpType{
     OP_EQUAL,
     OP_NOT,
     OP_ADD,
+	OP_SUB,
+	OP_MUL,
+	OP_DIV
 };
 
 enum VarType{
@@ -42,16 +46,15 @@ enum VarType{
 struct TreeNode {
     int nodeID;
     NodeType nodeType;
-
-    TreeNode *child = nullptr;
-    TreeNode *sibling = nullptr;
+    TreeNode* child  ;
+    TreeNode* sibling ;
 
     void addChild(TreeNode *);
     void addSibling(TreeNode *);
 
-    void genNodeId();//从根节点开始逐个赋Id 实现方式同学们可以自行修改
+    void genNodeId(TreeNode *);//从根节点开始逐个赋Id 实现方式同学们可以自行修改
 
-    void printAST();//打印语法树结点
+    void printAST(TreeNode *);//打印语法树结点
     /***
      * 以下的几个函数皆为在printAST过程中辅助输出使用
      * 同学们可以根据需要自己使用其他方法
